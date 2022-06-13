1622f3eeb1d510fb464901ed1890d0b4c4af7d6a
max <maksim.kalganov@gmail.com>
2022-06-13 20:43:02 +0300

    refactor utils

--- utils.py
+++ utils.py
@@ -3,8 +3,9 @@ from typing import Tuple
 from constants import PATH_TO_TRAIN, PATH_TO_TEST, SURVIVED, PATH_TO_PRED, PASSENGER_ID
 import pandas as pd
 import numpy as np
+import gin
 
-from parser import Parser
+from formatter import DatasetFormatter
 
 
 def read_train(path: str = PATH_TO_TRAIN) -> pd.DataFrame:
@@ -15,38 +16,36 @@ def read_test(path: str = PATH_TO_TEST) -> pd.DataFrame:
     return pd.read_csv(path)
 
 
-def split_train_test(df: pd.DataFrame, test_size: float = 0.2)\
-        -> Tuple[np.array, np.array, np.array, np.array]:
+def split_train_test(df: pd.DataFrame, train_test_split: float = 0.8) -> Tuple[np.array, np.array, np.array, np.array]:
     # TODO: make smart split (choose not randomly, but equally from each group)
-    assert test_size < 1, f"too big test_size = {test_size}"
-    test = df.sample(int(test_size * len(df)))
-    train = df.drop(test.index)
+    assert train_test_split < 1, f"too big train_test_split = {train_test_split}"
+    df_rand = df.sample(len(df))
+    train_size = int(train_test_split * len(df))
+    train = df_rand[:train_size]
+    test = df_rand[train_size:]
     assert len(test) + len(train) == len(df), f"wrong size test + train != df"
-    return train.drop(SURVIVED, axis=1).to_numpy(), train[SURVIVED].to_numpy(),\
+    return train.drop(SURVIVED, axis=1).to_numpy(), train[SURVIVED].to_numpy(), \
            test.drop(SURVIVED, axis=1).to_numpy(), test[SURVIVED].to_numpy()
 
 
-def prepare_train_data() \
-        -> Tuple[np.array, np.array, np.array, np.array]:
+def prepare_train_data() -> pd.DataFrame:
     full_dataset = read_train()
-    df = Parser().parse(full_dataset)
-    train_x, train_y, test_x, test_y = split_train_test(df)
-    return train_x, train_y, test_x, test_y
+    return DatasetFormatter().format(full_dataset)
 
 
-def prepare_test_data() -> Tuple[np.array, np.array]:
+def prepare_test_data() -> pd.DataFrame:
     full_dataset = read_test()
-    df = Parser().parse(full_dataset, test=True)
-    return full_dataset[PASSENGER_ID].to_numpy(), df.to_numpy()
+    return DatasetFormatter().format(full_dataset, test=True)
 
 
-def save_predictions(pass_id: np.array, pred: np.array, path: str = PATH_TO_PRED):
-    res_df = pd.DataFrame({PASSENGER_ID: pass_id, SURVIVED: pred})
+def save_predictions(pass_ids: np.array, preds: np.array, path: str = PATH_TO_PRED):
+    res_df = pd.DataFrame({PASSENGER_ID: pass_ids, SURVIVED: preds})
     res_df.to_csv(path, index=False)
 
 
-def custom_metric(ground_truth: np.array, predictions: np.array):
-    predictions = (predictions > 0.5).astype(int).reshape(-1)
+@gin.configurable
+def pr_rec_f1(ground_truth: np.array, predictions: np.array, threshold: float = 0.5):
+    predictions = (predictions > threshold).astype(int).reshape(-1)
     tp = sum(ground_truth & predictions)
     tn = sum((1 - ground_truth) & (1 - predictions))
     fp = sum(np.logical_xor(ground_truth, predictions) * predictions)
